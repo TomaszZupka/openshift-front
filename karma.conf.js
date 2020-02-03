e370d8d2f2554e9d6dfb1648c10a72e92e40db0c
Tomasz Zupka <tzupka@apreel.com>
2020-02-03 23:24:33 +0100

    chrome bin

--- karma.conf.js
+++ karma.conf.js
@@ -1,21 +1,22 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-// process.env.CHROME_BIN = require('puppeteer')
-process.env.CHROME_BIN = require('puppeteer').executablePath()
+// process.env.CHROME_BIN = require('puppeteer').executablePath();
+// require('puppeteer').connect({ browserWSEndpoint: 'ws://localhost:3000' });
 
 module.exports = function (config) {
   config.set({
     customLaunchers: {
       ChromeHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--headless',
-          '--disable-gpu',
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--remote-debugging-port=9222'
-        ]
+        base: 'WebDriver',
+        url: { hostname: 'localhost', port: 3000 }
+        // flags: [
+        //   '--headless',
+        //   '--disable-gpu',
+        //   '--no-sandbox',
+        //   '--disable-setuid-sandbox',
+        //   '--remote-debugging-port=9222'
+        // ]
       }
     },
     basePath: '',
