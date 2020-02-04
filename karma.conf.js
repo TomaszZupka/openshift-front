18561d1f8c9013b3f90b1bc17a1ce23e6ab464b7
Tomasz Zupka <tzupka@apreel.com>
2020-02-04 16:04:41 +0100

    pupet

--- karma.conf.js
+++ karma.conf.js
@@ -1,31 +1,27 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-// process.env.CHROME_BIN = require('puppeteer').executablePath();
-// require('puppeteer').connect({ browserWSEndpoint: 'ws://localhost:3000' });
-const webdriver = require('selenium-webdriver');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
-    customLaunchers: {
-      ChromeHeadless: {
-        base: 'SeleniumWebdriver',
-        browserName: 'Chrome',
-        getDriver: function () {
-          return new webdriver.Builder()
-            .forBrowser('chrome')
-            .usingServer('http://172.30.42.253:3000') // /wd/hub Docker is run using docker run -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome
-            .build()
-        }
-// flags: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-software-rasterizer', '--disable-dev-shm-usage', '--remote-debugging-port=9222']
-      }
-    },
+    // customLaunchers: {
+    //   ChromeHeadless: {
+    //     base: 'Chrome',
+    //     flags: [
+    //       '--headless',
+    //       '--disable-gpu',
+    //       '--no-sandbox',
+    //       '--disable-setuid-sandbox',
+    //       '--remote-debugging-port=9222'
+    //     ]
+    //   }
+    // },
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-selenium-webdriver-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -43,7 +39,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
