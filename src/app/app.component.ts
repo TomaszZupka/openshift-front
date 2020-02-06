9ea1a58088777014c9248e85b0e1b9d2eec9263d
Tomasz Zupka <tzupka@apreel.com>
2020-02-06 15:23:06 +0100

    click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'openshift-front';
+  test: string;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  onClick() {
+    this.httpClient.get<any>('/test')
+      .subscribe(data => {
+        console.log('data: ', data);
+        this.test = data;
+      });
+  }
 }
